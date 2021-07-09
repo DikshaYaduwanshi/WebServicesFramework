72523a548b0707f4cfdb854c2f8ea8eab321f6c1
DikshaYaduwanshi <82656519+DikshaYaduwanshi@users.noreply.github.com>
2021-07-09 23:27:59 +0530

    Add files via upload

--- WEB-INF/classes/com/thinking/machines/webrock/model/WebRockModel.java
+++ WEB-INF/classes/com/thinking/machines/webrock/model/WebRockModel.java
@@ -1,1 +1,19 @@
-ghdf
+package com.thinking.machines.webRock.model;
+import com.thinking.machines.webRock.pojo.*;
+import java.util.*;
+public class WebRockModel
+{
+private HashMap<String,Service> pathClassMap;
+public WebRockModel()
+{
+pathClassMap=new HashMap<>();
+}
+public void addToMap(String key,Service value)
+{
+this.pathClassMap.put(key,value);
+}
+public HashMap<String,Service> getMap()
+{
+return this.pathClassMap;
+}
+}
